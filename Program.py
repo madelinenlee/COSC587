cedf29478594a624619bc6151a7c98043be2156c
Mohammad Ali Yektaie <yektaie112358@gmail.com>
2018-10-01 13:29:20 -0400

    Added: Save and load weather dataset using pandas

--- Program.py
+++ Program.py
@@ -5,5 +5,15 @@ if __name__ == '__main__':
     _latitude = '38.98055649'
     _longitude = '-76.92222595'
 
-    a = weather.DailyWeatherObservationSet.load_from_api(_date, _latitude, _longitude)
-    print(a)
+    ds = weather.WeatherDataSet()
+    ds.add_day_observations(weather.DailyWeatherObservationSet.load_from_api(_date, _latitude, _longitude))
+    _latitude = '28.98055649'
+    _longitude = '-77.92222595'
+    ds.add_day_observations(weather.DailyWeatherObservationSet.load_from_api(_date, _latitude, _longitude))
+    ds.save('/Users/yektaie/Desktop/df.csv')
+
+    ds2 = weather.WeatherDataSet('/Users/yektaie/Desktop/df.csv')
+    ds2.load()
+    ds2.save('/Users/yektaie/Desktop/df2.csv')
+
+
--- providers/weather.py
+++ providers/weather.py
@@ -1,11 +1,83 @@
 import datetime
 import requests
 import json
+import pandas as pd
+
+
+class WeatherDataSet:
+    def __init__(self, path=None):
+        self.days = []
+        self.path = path
+
+    def load(self, path=None):
+        if (path is None) & (self.path is None):
+            raise ValueError('You should specify a path to load data set')
+        elif (path is not None) & (self.path is None):
+            self.path = path
+        elif (path is None) & (self.path is not None):
+            path = self.path
+
+        df = pd.read_csv(path)
+        _dict = {}
+
+        for index, row in df.iterrows():
+            lat_long_key = '{}__{}'.format(row['latitude'], row['longitude'])
+            day = None
+            if lat_long_key in _dict.keys():
+                day = _dict[lat_long_key]
+            else:
+                day = DailyWeatherObservationSet()
+                self.days.append(day)
+                _dict[lat_long_key] = day
+
+            day.entries.append(SingleWeatherObservation.load_from_csv(row))
+
+    def save(self, path=None):
+        if (path is None) & (self.path is None):
+            raise ValueError('You should specify a path to save data set')
+        elif (path is not None) & (self.path is None):
+            self.path = path
+        elif (path is None) & (self.path is not None):
+            path = self.path
+
+        data = []
+        for day in self.days:
+            for record in day.entries:
+                data.append(record.to_dictionary())
+
+        columns = ['time', 'latitude', 'longitude', 'day_night_index', 'temperature', 'feels_like', 'heat_index',
+                   'dew_point', 'relative_humidity', 'condition', 'pressure', 'visibility', 'wind_speed',
+                   'wind_direction', 'uv_index']
+        df = pd.DataFrame(data, columns=columns)
+
+        df.to_csv(path)
+
+    def add_day_observations(self, day_data):
+        self.days.append(day_data)
+
+    def get_day_observations(self, date, latitude, longitude):
+        result = None
+
+        for day in self.days:
+            if (day.latitude == latitude) & (day.longitude == longitude):
+                result = day
+                break
+
+        return result
+
+    def as_pandas_dataframe(self):
+        if self.path is None:
+            raise ValueError('You should specify a path to load data set')
+
+        return pd.read_csv(self.path)
 
 
 class DailyWeatherObservationSet:
-    def __init__(self, entries, date, latitude, longitude):
+    def __init__(self, entries=None, date=None, latitude=None, longitude=None):
         self.entries = entries
+        if self.entries is None:
+            self.entries = []
+
         self.date = date
         self.latitude = latitude
         self.longitude = longitude
@@ -18,11 +90,11 @@ class DailyWeatherObservationSet:
 
         url = url.format(latitude, longitude, date, date)
         response = requests.get(url)
-        json_data = json.loads(response.text)
+        response_data = json.loads(response.text)
         observations = []
 
-        for observation in json_data['observations']:
-            o = SingleWeatherObservation.load_from_api_response(observation)
+        for observation in response_data['observations']:
+            o = SingleWeatherObservation.load_from_api_response(observation, latitude, longitude)
             observations.append(o)
 
         return DailyWeatherObservationSet(observations, date, latitude, longitude)
@@ -86,7 +158,7 @@ class DailyWeatherObservationSet:
 
 
 class SingleWeatherObservation:
-    def __init__(self):
+    def __init__(self, latitude, longitude):
         self.date_time = 0
         self.day_night = ''
         self.temperature = 0
@@ -100,6 +172,8 @@ class SingleWeatherObservation:
         self.wind_speed = 0
         self.wind_direction = ''
         self.uv_index = 0
+        self.latitude = latitude
+        self.longitude = longitude
 
     def get_date_time(self):
         return self.date_time
@@ -141,8 +215,8 @@ class SingleWeatherObservation:
         return self.wind_direction
 
     @classmethod
-    def load_from_api_response(cls, observation):
-        result = SingleWeatherObservation()
+    def load_from_api_response(cls, observation, latitude, longitude):
+        result = SingleWeatherObservation(latitude, longitude)
 
         result.date_time = observation['valid_time_gmt']
         result.day_night = observation['day_ind']
@@ -159,3 +233,46 @@ class SingleWeatherObservation:
         result.uv_index = observation['uv_index']
 
         return result
+
+    @classmethod
+    def load_from_csv(cls, observation):
+        latitude = observation['latitude']
+        longitude = observation['longitude']
+
+        result = SingleWeatherObservation(latitude, longitude)
+
+        result.date_time = observation['time']
+        result.day_night = observation['day_night_index']
+        result.temperature = observation['temperature']
+        result.feels_like_temperature = observation['feels_like']
+        result.heat_index = observation['heat_index']
+        result.dew_point = observation['dew_point']
+        result.relative_humidity = observation['relative_humidity']
+        result.condition = observation['condition']
+        result.pressure = observation['pressure']
+        result.vis = observation['visibility']
+        result.wind_speed = observation['wind_speed']
+        result.wind_direction = observation['wind_direction']
+        result.uv_index = observation['uv_index']
+
+        return result
+
+    def to_dictionary(self):
+        return {
+            'time': self.date_time,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'day_night_index': self.day_night,
+            'temperature': self.temperature,
+            'feels_like': self.feels_like_temperature,
+            'heat_index': self.heat_index,
+            'dew_point': self.dew_point,
+            'relative_humidity': self.relative_humidity,
+            'condition': self.condition,
+            'pressure': self.pressure,
+            'visibility': self.vis,
+            'wind_speed': self.wind_speed,
+            'wind_direction': self.wind_direction,
+            'uv_index': self.uv_index
+        }
+
