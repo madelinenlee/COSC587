4e2e5b80f0744e552bba7e879da26b05c902bc43
dctodka <tspaightm@gmail.com>
2018-10-03 21:56:34 -0400

    Added code to clean pro-football-reference.csv and cleaned version of data after first pass

---
+++ clean.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def cleanProFootballReference(df):
+
+	# Only keep offensive positions
+	off_pos = "QB|WR|RB|TE|K|FB|HB"
+	df = df[df.position.str.contains(off_pos, na=False)]
+
+	# Narrow data to games since 2009 not including 2018 games
+	df = df[2009 <= df.year_id]
+	df = df[df.year_id <= 2017]
+	
+	# Remove unnecessary columns (defensive / punting)
+	df = df.drop(["def_int", "def_int_yds", "game_result", "punt",
+		          "punt_blocked", "punt_yds", "sacks", "safety_md",
+		          "tackles_assists", "tackles_solo",], axis=1)
+
+	return df
+
+
+if __name__ == "__main__":
+
+	# Pro Football Reference
+	filename = "data\\pro-football-reference.csv"
+	pfr = pd.read_csv(filename)
+
+	pfr = cleanProFootballReference(pfr)
+
+	pfr.to_csv("cleaned_data\\pro-football-reference.csv", index=False)
+
+	
